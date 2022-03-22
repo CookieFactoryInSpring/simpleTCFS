d3191b9d6b0b7a01e8ff49cbbb6051d892090701
Philippe Collet <Philippe.Collet@unice.fr>
2022-03-22 10:18:23 +0100

    remove unused import in boot app

--- backend/src/main/java/fr/univcotedazur/simpletcfs/SimpleTcfsServer.java
+++ backend/src/main/java/fr/univcotedazur/simpletcfs/SimpleTcfsServer.java
@@ -2,7 +2,6 @@ package fr.univcotedazur.simpletcfs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
